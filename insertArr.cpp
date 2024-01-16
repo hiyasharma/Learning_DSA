4fe01b26c851233f9a6eb4ab6130734bd5f6ec86
Hiya Sharma <hiya.sharma2021@vitbhopal.ac.in>
2024-01-16 21:21:32 +0530

    Insert Operation in Array

---
+++ insertArr.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int insertElement(int arr[], int n, int key, int capacity)
+{
+    if (n >= capacity)
+    {
+        return n;
+    }
+
+    arr[n] = key;
+    return (n+1);
+}
+
+int main()
+{
+    int arr[20]={12, 16, 20, 40, 50, 70};
+
+    int capacity = sizeof(arr)/sizeof(arr[0]);
+
+    int n = 6;
+    int i, key = 26;
+
+
+    cout<<"Before Insertion:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    n = insertElement(arr, n, key, capacity);
+
+    cout<<"\nAfter Insertion:"<<endl;
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
